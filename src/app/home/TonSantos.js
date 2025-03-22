e97b5028675ad6b9fecbc9c5929161740e1275fd
tonsantosss <tonsantoss@yahoo.com.br>
2025-03-21 22:19:37 -0300

    Eu TonSantos acredito na virada

---
+++ src/app/home/TonSantos.js
@@ -0,0 +1,11 @@
+class TonSantos {
+    constructor() {
+        this.nome = "Ton Santos";
+    }
+
+    saudacao() {
+        return "Eu TonSantos acredito na virada!";
+    }
+}
+
+export default TonSantos;
